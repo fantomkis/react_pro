3ac2beba5248e9780f05a0046bb0e37c026371c5
Pavlo Kysliak <pavel.kis0505@gmail.com>
2023-04-24 13:28:00 +0300

    add routs in Main

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,10 @@
 
 
-import { Route, Routes } from 'react-router-dom';
-import Family from './Family/Family';
-import Home from '../pages/Home';
-import Friends from './Friends/Friends';
-import CloseFriends from './CloseFriends/CloseFriends';
-import JustPeople from './JustPeople/JustPeople';
 import Footer from './Footer/Footer';
 import Header from './Header.tsx/Header';
 import s from './App.module.css';
 import Main from './Main/Main';
-import { type } from 'os';
+
 
 function App() {
   return (
@@ -21,13 +15,7 @@ function App() {
       
         
          <Main/>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/friends' element={<Friends />} />
-          <Route path='/family' element={<Family />} />
-          <Route path='/closefr' element={<CloseFriends />} />
-            <Route path='/justpeple' element={<JustPeople />} />
-        </Routes>
+     
        
         </div>
       <Footer/>
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,25 @@
 
+import { Route, Routes } from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar';
+import Home from '../../pages/Home';
+import Friends from '../Friends/Friends';
+import Family from '../Family/Family';
+import CloseFriends from '../CloseFriends/CloseFriends';
+import JustPeople from '../JustPeople/JustPeople';
+import s from './Main.module.css';
+import Notfound from '../Notfound/Notfound';
 function Main() {
     return (
-        <main>
-            <Sidebar></Sidebar>
+        <main className={s.wrap}>
+            <Sidebar />
+                    <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/friends' element={<Friends />} />
+          <Route path='/family' element={<Family />} />
+          <Route path='/closefr' element={<CloseFriends />} />
+                <Route path='/justpeple' element={<JustPeople />} />
+                <Route path='*' element={<Notfound/>} />
+        </Routes>
         </main>
     );
 }
